02663540108ea0e3e5231371a21b59d13f4d5d38
Kent <767188747@qq.com>
2018-05-09 23:30:24 +0800

    Layout

---
+++ src/Panel1.java
@@ -0,0 +1,60 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel1 {
+    public static void main(String[] args) {
+        Panel1 gui = new Panel1();
+        gui.go();
+    }
+    /*
+    public void go() {  //BorderLayout
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.darkGray);
+
+        JButton button = new JButton("Shock me");
+        JButton buttonTwo = new JButton("bliss");
+
+        panel.add(button);
+        panel.add(buttonTwo);
+
+        frame.getContentPane().add(BorderLayout.EAST, panel);
+        frame.setSize(250, 200);
+        frame.setVisible(true);
+    }
+    */
+    /*
+    public void go() {  //框架默认为BoxLayout，面板默认为FlowLayout
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.darkGray);
+
+        JButton button = new JButton("Shock me");
+        JButton buttonTwo = new JButton("bliss");
+
+        panel.add(button);
+        panel.add(buttonTwo);
+
+        frame.getContentPane().add(panel);
+        frame.setSize(250, 200);
+        frame.setVisible(true);
+    }
+    */
+
+    public void go() {  //BoxLayout
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.darkGray);
+
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JButton button = new JButton("Shock me");
+        JButton buttonTwo = new JButton("bliss");
+        panel.add(button);
+        panel.add(buttonTwo);
+        frame.getContentPane().add(BorderLayout.EAST, panel);
+        frame.setSize(250, 200);
+        frame.setVisible(true);
+    }
+
+}
